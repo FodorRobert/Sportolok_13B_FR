e5712775adbbe57e9024106abadad75fccce9c81
fodorr <fodorr@kkszki.hu>
2026-09-23 09:27:07 +0200

    Project started

---
+++ portolok_13B_FR/Controllers/EredmenyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+namespace portolok_13B_FR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EredmenyController : ControllerBase
+    {
+
+        
+
+    }
+}
---
+++ portolok_13B_FR/Models/Eredmeny.cs
@@ -0,0 +1,12 @@
+﻿namespace portolok_13B_FR.Models
+{
+    public class Eredmeny
+    {
+
+        public string? Competition { get; set; }
+        public string? Description { get; set; }
+        public DateTime? ResultTime { get; set; }
+        public DateTime? UpdateTime { get; set; }
+
+    }
+}
